370f6e549d911047ed0270359b62c328d5ad61b2
MacMini <macmini@MacMiniui-Macmini.local>
2022-06-08 15:55:12 +0900

    test

---
+++ src/routes/Router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Coins from "./Coins";
+import Coin from "./Coin";
+function Router() {
+  <BrowserRouter>
+    <Switch>
+      <Route path="/:coinId">
+        <Coin />
+      </Route>
+      <Route path="/">
+        <Coins />
+      </Route>
+    </Switch>
+  </BrowserRouter>;
+}
+
+export default Router;
