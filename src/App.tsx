ce17b004d9f069abaf6dc4d73c35a751a71cff43
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2022-06-09 00:32:34 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
-
+import { ReactQueryDevtools } from "react-query/devtools";
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&display=swap');
 html, body, div, span, applet, object, iframe,
@@ -64,6 +64,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
